4835f6c0969c418bb4f582c25d3e30807d3dc38c
Igor Kalayda <iekalayda@gmail.com>
2024-03-04 15:15:56 +0300

    Add config, update ui

--- mainwindow.py
+++ mainwindow.py
@@ -2,16 +2,20 @@ from PyQt5 import QtWidgets, uic
 
 import sys
 
+from signal_ctrl import SignalCtrl
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         uic.loadUi("mainwindow.ui", self)
 
-        self.widget_rx.canvas.ax.plot([0, 1, 2, 3, 4, 5, 6, 7, 8], [0, 1, 2, 2, 1, 0, 3, 1, 0], "r")
-        self.widget_rx.canvas.draw()
+        self.signal_control = SignalCtrl()
 
-        self.widget_tx.canvas.ax.plot([0, 1, 2, 3, 4, 5, 6, 7, 8], [0, 2, 2, 3, 0, 0, 1, 1, 0], "g")
-        self.widget_tx.canvas.draw()
+        self.widget_lp_rx.canvas.ax.plot([0, 1, 2, 3, 4, 5, 6, 7, 8], [0, 1, 2, 2, 1, 0, 3, 1, 0], "r")
+        self.widget_lp_rx.canvas.draw()
+
+        self.widget_lp_tx.canvas.ax.plot([0, 1, 2, 3, 4, 5, 6, 7, 8], [0, 2, 2, 3, 0, 0, 1, 1, 0], "g")
+        self.widget_lp_tx.canvas.draw()
 
         
         # self.widget_DV.canvas.draw()
--- signal_ctrl.py
+++ signal_ctrl.py
@@ -1,10 +1,57 @@
-from PyQt5.QtCore import QObject, QThread
+from PyQt5.QtCore import QObject
 from sdr_ctrl import SDRCtrl
+import json
 
 import numpy as np
 
 class SignalCtrl(QObject):
-    def __init__(self, parent=None):
-        sdr = SDRCtrl()
-        tx_signal = np.array()
-        rx_signal = np.array()
+    def __init__(self, parent=None) -> None:
+        self.sdr_control = None
+
+        json_data = json.load(open("config.json"))
+        self.signals = {
+            obj: 
+            (
+                json_data["signals"][obj]["name"],
+                json_data["signals"][obj]["seq_type"],
+                json_data["signals"][obj]["txt_path"]
+            )
+            for obj in json_data["signals"]
+            }
+
+        self.tx_signal = None
+        self.rx_signal = None
+    
+    def set_tx(self, tx_signal: np.array) -> None:
+        self.tx_signal = tx_signal
+    
+    def get_rx(self) -> np.array:
+        self.rx_signal = self.sdr_control.get_rx()
+    
+    def set_tx_from_list(self, signal_name: str) -> None:
+        pass
+
+    def get_signal_list(self) -> dict:
+        return self.signals
+    
+    def listen(self) -> int:
+        if self.sdr_control is None:
+            try:
+                self.sdr_control = SDRCtrl()
+            except:
+                print("SDR Control can't create")
+                return -1
+        return 0
+
+    def send_and_listen(self) -> int:
+        if self.sdr_control is None:
+            try:
+                self.sdr_control = SDRCtrl()
+            except:
+                print("SDR Control can't create")
+                return -1
+        return 0
+
+    def imitate(self, sigma: int, delay: int) -> None :
+        noise_complex = np.random.normal(size=len(self.rx_signal))*np.exp(-1j*2*np.pi*np.random.randn(len(self.rx_signal)))
+        noise_complex = sigma*noise_complex/(max(noise_complex.real) + 1j*max(noise_complex.imag))
